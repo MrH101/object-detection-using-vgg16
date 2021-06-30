697d7a148e4950df296e740dae18aee3822d025d
MrH101 <42887874+MrH101@users.noreply.github.com>
2021-06-30 14:30:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,69 +10,74 @@ from tensorflow.keras.applications.vgg16 import decode_predictions
 from tensorflow.keras.applications.vgg16 import VGG16
 from PIL import Image
 
-st.title("vgg16 object detection")
-def upload_vid():
-  vid_upload = st.file_uploader("Upload Video for object classification", type=["mp4","avi"])# SELECTING A SPECIFIC VIDEO TYPE TO UPLOAD
-  vids = tempfile.NamedTemporaryFile(delete=False)
-  if vid_upload is not None:  
-    vids.write(vid_upload.read())
-  return vids.name 
-      
-def framing(videoPath):
-  count = 0
-  cap = cv2.VideoCapture(videoPath)   
+st.title("Object Detection Using VGG16")
 
+def uploadFile():
+  vid_file = st.file_uploader("Upload a video", type=["mp4", "mov","avi"])
+  tempVideo = tempfile.NamedTemporaryFile(delete=False) 
+  if vid_file is not None: 
+    tempVideo.write(vid_file.read())
+  return tempVideo.name
+
+def splitVideo(videoPath):
+  count = 0
+  cap = cv2.VideoCapture(videoPath)
   frameRate = cap.get(5) 
   tempImage = tempfile.NamedTemporaryFile(delete=False) 
   x=1
-  while(cap.isOpened()):
+
+  while (cap.isOpened()):
     frameId = cap.get(1) 
     ret, frame = cap.read()
-    if ret:    
-      tempImage = videoPath.split('.')[0] +"_frame%d.jpg" % count;
-      count+=1
+    if (ret != True):
+      break
+    if (frameId % math.floor(frameRate) == 0):
+      tempImage = videoPath.split('.')[0] +"_frame%d.jpg" % count;count+=1
       cv2.imwrite(tempImage, frame)
       frames.append(tempImage)
   cap.release() 
   return frames,count
+
 def classifyObjects():  
-  model = VGG16() 
-  format_string = [] 
-  frames,count = framing(videoFile)
+  model = VGG16()
+  classify = []
+  frames,count = splitVideo(videoFile)
+  print(count)
 
   for i in range(count):    
     image = load_img(frames[i], target_size=(224, 224)) 
     image = img_to_array(image)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
     image = preprocess_input(image)   
-    prediction = model.predict(image)  
-    label = decode_predictions(prediction)
-    label = label[0][0] 
+    img_pred = model.predict(image)
+    label = decode_predictions(img_pred)    
+    label = label[0][0]
     result =  label[1]
-    format_string.append(result)
-  return format_string
+    classify.append(result)
+  return classify
 
-user_input = st.text_input("Enter object to be searched: ")
-videoFile = upload_vid()
-
-def compare_frames(object_):
-  indexes = []
-  format_string = classifyObjects()
-  if object_ in format_string:
-    for i in range(len(format_string)):
-      if format_string[i] == object_:
-        index = format_string.index(object_)
-        indexes.append(index)
+def searchInFrames(object_):
+  indeces = []
+  classifications = classifyObjects()
+  if object_ in classifications:
+    for i in range(len(classifications)):
+      if classifications[i] == object_:
+        index = classifications.index(object_)
+        indeces.append(index)
         filePath = frames[index]
-        img = load_img(filePath, target_size = (224, 224, 224))
+        img = load_img(filePath, target_size = (224, 224))
         detected_paths.append(filePath)
-    for i in range(len(indexes)):
+    for i in range(len(indeces)):
       st.image(frames[i], width=224)
   else:
-    st.write("Object not found!")
+    st.write("The object NOT FOUND.")
 
-if st.button('search'):  
+videoFile = uploadFile()
+user_input = st.text_input("Enter the object you are searching: ")
+
+if st.button('Search'):
+  classifyObjects(videoFile)  
   frames =[]
   detected_paths = []
-  compare_frames(user_input)
+  searchInFrames(user_input)
   st.write("")
